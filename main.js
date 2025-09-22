3d437fe69766370c8ebc487b8438c29f2754f804
Beartil Rajan <229802592+beartilrajan@users.noreply.github.com>
2025-09-22 15:15:37 +0000

    update

--- main.js
+++ main.js
@@ -1,7 +1,45 @@
-console.log("hello world");
 
-function getcomputerchoice(max){
-    return Math.floor(Math.random()* max);
+let compchoice = Math.floor(Math.random() * 3);
+console.log(compchoice);
+
+let message = prompt("Rock Paper Scissor Game \n - Rock = 0\n - Paper = 1\n - Scissor = 2");
+
+let humanchoice = parseInt(message);
+console.log(humanchoice);
+
+let Rock = 0;
+let Paper = 1;
+let Scissor = 2;
+
+function playRound(a,b)
+{
+    if(a===b){
+        console.log("Draw");
+    }
+    else if(a === Rock){
+        if(b === Paper){
+            console.log("Computer Wins: Paper beats Rock");
+        }
+        else{
+            console.log("Human Wins: Rock beats Scissor");
+        }
+    }
+    else if(a === Paper){
+        if(b === Scissor){
+            console.log("Computer Wins: Scissor beats Paper");
+        }
+        else{
+            console.log("Human Wins: Rock beats Scissor");
+        }
+    }
+    else if(a === Scissor){
+        if(b === Rock){
+            console.log("Computer Wins: Rock beats Scissor");  
+        }
+        else{
+            console.log("Human Wins: Scissor beats Paper");
+        }
+    }
 }
 
-console.log(getcomputerchoice(3));
+playRound(humanchoice,compchoice);
